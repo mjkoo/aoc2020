49d7920703dbb1ba1f6fc2c12901294be11dfa14
Maxwell Koo <mjkoo90@gmail.com>
2020-12-01 01:53:48 -0700

    Day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code Solutions 2020
+
+Attempting to solve all challenges in Rust
---
+++ day1/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let filename = args.get(1).expect("No file specified");
+    let file = File::open(filename).expect("Could not open input file");
+    let reader = BufReader::new(file);
+
+    let nums: HashSet<_> = reader
+        .lines()
+        .map(|s| s.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    // Step 1
+    for x in &nums {
+        let y = 2020 - x;
+        if nums.contains(&y) {
+            println!("{}", x * y);
+            break;
+        }
+    }
+
+    // Step 2
+    for x in &nums {
+        for y in &nums {
+            let z = 2020 - x - y;
+            if nums.contains(&z) {
+                println!("{}", x * y * z);
+                return;
+            }
+        }
+    }
+}
